6f5a9f729368e3be710c58c37b37b6cda5b06ceb
danischm <danischm@cisco.com>
2022-10-14 16:44:35 +0200

    Bump qos policy module version

--- main.tf
+++ main.tf
@@ -1168,7 +1168,7 @@ module "aci_bfd_interface_policy" {
 
 module "aci_qos_policy" {
   source  = "netascode/qos-policy/aci"
-  version = ">= 0.1.1"
+  version = ">= 0.1.2"
 
   for_each    = { for pol in lookup(lookup(local.tenant, "policies", {}), "qos", []) : pol.name => pol if lookup(local.modules, "aci_qos_policy", true) }
   tenant      = module.aci_tenant[0].name
